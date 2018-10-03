6fd796eac8c38133356992944a57b71663b9f708
Erica Wright <ericaewright@gmail.com>
2018-10-02 17:29:12 -0700

    add more attributes to the projects table and add to form, remove dotenv

---
+++ migrations/2018-10-02-235451_add_attributes_to_projects/down.sql
@@ -0,0 +1,9 @@
+ALTER TABLE projects 
+DROP COLUMN status,
+DROP COLUMN position,
+DROP COLUMN requester,
+DROP COLUMN url,
+DROP COLUMN assigned_to,
+DROP COLUMN date_finished,
+DROP COLUMN created_at,
+DROP COLUMN updated_at;
--- src/models.rs
+++ src/models.rs
@@ -1,16 +1,25 @@
 use super::schema::projects;
+use chrono::NaiveDateTime;
 
 #[derive(Queryable, Debug, Serialize)]
 pub struct Project {
     pub id: i32,
     pub title: String,
     pub body: String,
+    pub status: String,
+    pub position: Option<i32>,
+    pub requester: String,
+    pub url: Option<String>,
+    pub assigned_to: Option<String>,
+    pub date_finished: Option<NaiveDateTime>,
+    pub created_at: NaiveDateTime,
+    pub updated_at: NaiveDateTime,
 }
-
 
 #[derive(Insertable, Deserialize)]
 #[table_name = "projects"]
 pub struct NewProject {
     pub title: String,
     pub body: String,
+    pub requester: String,
 }
